b170f225b2c53b054c90656e90a72c63453f4ca0
Amal-S-2003 <amalshibu379@gmail.com>
2024-11-29 11:52:46 +0530

    Some pages created

---
+++ script.js
@@ -0,0 +1,11 @@
+// Side menu
+const menu=document.getElementById("menu")
+const nav=document.getElementById("nav")
+
+function openMenu() {
+  menu.style.right = "0";
+}
+
+function closeMenu() {
+  menu.style.right = "-300px";
+}
